28faa64c2cac9276fb5eaed0e0bc49a66fc6f7cc
Parinitha Kompala <77102792+Parinitha-kompala@users.noreply.github.com>
2021-08-23 10:08:23 -0400

    Create healthcare ai.R

---
+++ healthcare ai.R
@@ -0,0 +1,7 @@
+library(healthcareai)
+quick_models <- machine_learn(pima_diabetes, patient_id, outcome = diabetes)
+str(pima_diabetes)
+quick_models
+predictions <- predict(quick_models)
+predictions
+plot(predictions)
