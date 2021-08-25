1985e85296ed452b0e5e4b02c217d95c730b61fb
Parinitha Kompala <parinitha.kompala.gr@dartmouth.edu>
2021-08-24 21:02:52 -0400

    Update healthcare ai.R

--- healthcare ai.R
+++ healthcare ai.R
@@ -1,16 +1,38 @@
+#getting the library loaded
 library(healthcareai)
-quick_models <- machine_learn(pima_diabetes, patient_id, outcome = diabetes)
+#analysising the data 
+pima_diabetes #inbuit dataframe
+dim(pima_diabetes)
+summary(pima_diabetes)
 str(pima_diabetes)
+#Machine learning
+#prediction_1
+head(pima_diabetes)
+names(pima_diabetes)
+
+quick_models <- machine_learn(pima_diabetes, patient_id, outcome = diabetes)
 quick_models
 predictions <- predict(quick_models)
 predictions
 plot(predictions)
-#checking Git
+#prediction_2(diabetic based on plasma_glucose)
+a_quick_models <- machine_learn(pima_diabetes, plasma_glucose, outcome = diabetes)
+a_quick_models
+a_predictions <- predict(quick_models)
+a_predictions
+plot(a_predictions)
 quick_models %>%
   predict(outcome_groups = 2) %>%
   plot()
+a_quick_models %>%
+  predict(outcome_groups = 2) %>%
+  plot()
+#data profiling
+missingness(pima_diabetes)
 missingness(pima_diabetes) %>%
   plot()
+#data preparation
+prep_data
 split_data <- split_train_test(d = pima_diabetes,
                                outcome = diabetes,
                                p = .9,
@@ -18,6 +40,7 @@ split_data <- split_train_test(d = pima_diabetes,
 prepped_training_data <- prep_data(split_data$train, patient_id, outcome = diabetes,
                                    center = TRUE, scale = TRUE,
                                    collapse_rare_factors = FALSE)
+#model training
 models <- tune_models(d = prepped_training_data,
                       outcome = diabetes,
                       tune_depth = 25,
@@ -25,16 +48,22 @@ models <- tune_models(d = prepped_training_data,
 evaluate(models, all_models = TRUE)
 models["Random Forest"] %>%
   plot()
+#faster model training
 untuned_rf <- flash_models(d = prepped_training_data,
                            outcome = diabetes,
                            models = "RF",
                            metric = "PR")
+#model interpretation
+#interpret
 interpret(models) %>%
   plot()
+#variable importance
 get_variable_importance(models) %>%
   plot()
+# exploring
 explore(models) %>%
   plot()
+#prediction
 predict(models)
 test_predictions <-
   predict(models,
@@ -43,11 +72,15 @@ test_predictions <-
   )
 # > Prepping data based on provided recipe
 plot(test_predictions)
-
+#Saving, Moving, and Loading Models
 save_models(models, file = "my_models.RDS")
 models <- load_models("my_models.RDS")
+#regression models
 regression_models <- machine_learn(pima_diabetes, patient_id, outcome = age)
 summary(regression_models)
+a_regression_models <- machine_learn(pima_diabetes, pregnancies, outcome = age)
+summary(a_regression_models)
+#prediction on a hypothetical new patient 1
 new_patient <- data.frame(
   pregnancies = 0,
   plasma_glucose = 80,
@@ -58,3 +91,14 @@ new_patient <- data.frame(
   pedigree = .2,
   diabetes = "N")
 predict(regression_models, new_patient)
+#prediction on a hypothetical new patient 2
+new_patient_2 <- data.frame(
+  pregnancies = 3,
+  plasma_glucose = 130,
+  diastolic_bp = 60,
+  skinfold = 24,
+  insulin = NA,
+  weight_class = "???",
+  pedigree = .2,
+  diabetes = "N")
+predict(regression_models, new_patient_2)
